2447361ef76a2600594607c4e8303435f802707e
Kayasit Rugtepa <smartendude@gmail.com>
2016-05-17 21:34:36 +0700

    add prob to result page

--- bio_web/bio_web/main/views.py
+++ bio_web/bio_web/main/views.py
@@ -46,7 +46,8 @@ def homepage(request):
                                 print "FINAL RESULT: " + data_with_uid[0]
                                 get_output = True
                                 return render(request, 'result.html', {
-                                	"result": data_with_uid[0]
+                                	"result": data_with_uid[0],
+                                    'prob': data_with_uid[2]
                                 	})
         except :
             print 'Unable to connect'
